2ea9a50735af760546f0052ff98198871e9c3708
thebrownfoxx <hamuelxd@gmail.com>
2026-05-15 10:35:10 +0800

    compiler::lexer::Cursor: use LinkedList for buffer

--- compiler/lexer/src/cursor.rs
+++ compiler/lexer/src/cursor.rs
@@ -1,10 +1,10 @@
-use std::str::Chars;
+use std::{collections::LinkedList, str::Chars};
 
 #[derive(Debug)]
 pub(crate) struct Cursor<'source> {
     characters: Chars<'source>,
     index: usize,
-    buffer: Option<char>,
+    buffer: LinkedList<char>,
 }
 
 impl<'source> Cursor<'source> {
@@ -12,7 +12,7 @@ impl<'source> Cursor<'source> {
         Self {
             characters: source.chars(),
             index: 0,
-            buffer: None,
+            buffer: LinkedList::new(),
         }
     }
 
@@ -21,13 +21,13 @@ impl<'source> Cursor<'source> {
     }
 
     pub fn pop(&mut self) -> Option<char> {
-        let character = self.buffer.take().or_else(|| self.characters.next())?;
+        let character = self.buffer.pop_front().or_else(|| self.characters.next())?;
         self.index += character.len_utf8();
         Some(character)
     }
 
-    pub const fn push(&mut self, character: char) {
-        self.buffer = Some(character);
+    pub fn push(&mut self, character: char) {
+        self.buffer.push_front(character);
         self.index -= character.len_utf8();
     }
 }
