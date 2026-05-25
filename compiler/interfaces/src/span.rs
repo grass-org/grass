049ade519ec4387a14a484ce9649b50a1c2e4275
thebrownfoxx <hamuelxd@gmail.com>
2026-05-25 20:57:18 +0800

    compiler::interfaces::Span: expose end property

--- compiler/interfaces/src/span.rs
+++ compiler/interfaces/src/span.rs
@@ -3,3 +3,10 @@ pub struct Span {
     pub start: usize,
     pub length: usize,
 }
+
+impl Span {
+    /// End exclusive
+    pub const fn end(self) -> usize {
+        self.start + self.length
+    }
+}
