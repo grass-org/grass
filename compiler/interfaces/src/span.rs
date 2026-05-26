afb29371e2113a94d7f7e751e61efdcf88b0a177
thebrownfoxx <hamuelxd@gmail.com>
2026-05-26 21:01:42 +0800

    compiler::interfaces::Span: impl Display

--- compiler/interfaces/src/span.rs
+++ compiler/interfaces/src/span.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Display, Formatter};
+
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Debug)]
 pub struct Span {
     pub start: usize,
@@ -8,5 +10,11 @@ impl Span {
     /// End exclusive
     pub const fn end(self) -> usize {
         self.start + self.length
+    }
+}
+
+impl Display for Span {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} -> {}", self.start, self.end())
     }
 }
