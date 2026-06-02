a0a8ee1fba6ebcdae6b826215173d09df5951dd5
thebrownfoxx <hamuelxd@gmail.com>
2026-06-02 14:17:07 +0800

    compiler::lexer: impl Display for Token

--- compiler/lexer/src/token/literal/character.rs
+++ compiler/lexer/src/token/literal/character.rs
@@ -1,10 +1,25 @@
 use crate::token::{PushTokenCharacterResult, TokenBuilder};
 use crate::Token;
+use std::fmt::{self, Display, Formatter};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Debug)]
 pub struct CharacterLiteral {
     pub symbol: char,
     pub escaped: bool,
+}
+
+impl Display for CharacterLiteral {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let CharacterLiteral { symbol, escaped } = self;
+
+        write!(f, "'")?;
+
+        if *escaped {
+            write!(f, "\\")?;
+        }
+
+        write!(f, "{symbol}")
+    }
 }
 
 pub(crate) struct CharacterLiteralBuilder {
--- compiler/lexer/src/token/mod.rs
+++ compiler/lexer/src/token/mod.rs
@@ -7,6 +7,7 @@ mod single_character;
 mod tokenizer;
 
 pub use literal::*;
+use std::fmt::{Display, Formatter};
 
 pub(crate) use builder::*;
 pub(crate) use identifier::*;
@@ -45,4 +46,26 @@ pub enum Token {
     Identifier(String),
 
     Invalid,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::NewLine => write!(f, "\\n"),
+            Token::OpenParenthesis => write!(f, "("),
+            Token::CloseParenthesis => write!(f, ")"),
+            Token::OpenBracket => write!(f, "["),
+            Token::CloseBracket => write!(f, "]"),
+            Token::OpenBrace => write!(f, "{{"),
+            Token::CloseBrace => write!(f, "}}"),
+            Token::Comma => write!(f, ","),
+            Token::Colon => write!(f, ":"),
+            Token::Operator(value) => value.fmt(f),
+            Token::NumericLiteral(value) => value.fmt(f),
+            Token::CharacterLiteral(value) => value.fmt(f),
+            Token::StringLiteral(value) => value.fmt(f),
+            Token::Identifier(value) => value.fmt(f),
+            Token::Invalid => write!(f, "{{invalid_token}}"),
+        }
+    }
 }
