5a13451f8c1968ad0fa0b1192683d7f53ed5fbad
thebrownfoxx <hamuelxd@gmail.com>
2026-06-08 19:43:31 +0800

    compiler::lexer: rename numeric and character modules for consistency

--- compiler/lexer/src/lex/mod.rs
+++ compiler/lexer/src/lex/mod.rs
@@ -1,11 +1,11 @@
-mod character_literal;
-mod numeric_literal;
+mod character;
+mod numeric;
 mod single_character;
 mod operator;
 mod new_line;
 
-pub(crate) use character_literal::*;
-pub(crate) use numeric_literal::*;
+pub(crate) use character::*;
+pub(crate) use numeric::*;
 pub(crate) use single_character::*;
 pub(crate) use operator::*;
 pub(crate) use new_line::*;
