d2f3990ff7db8b83f7bd3c1140b71cab18e600ff
thebrownfoxx <hamuelxd@gmail.com>
2026-06-25 19:59:27 +0800

    compiler::interfaces::SyntaxKind: add AtomicExpression

--- compiler/interfaces/src/syntax_kind.rs
+++ compiler/interfaces/src/syntax_kind.rs
@@ -1,6 +1,7 @@
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Debug)]
 pub enum SyntaxKind {
     Expression,
+    AtomicExpression,
     Operator,
     OpenParenthesis,
     CloseParenthesis,
